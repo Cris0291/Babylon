6d40a5461b608fe46217ed62dda9b486d47ce4df
Cris0291 <cris0291@gmail.com>
2025-03-10 08:44:48 -0600

    Add the api project

--- Babylon/src/API/Babylon.Api/obj/Debug/net8.0/Babylon.Api.AssemblyInfo.cs
+++ Babylon/src/API/Babylon.Api/obj/Debug/net8.0/Babylon.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Babylon.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4acbd518bfa7bb6ff5d38297d1eef1e88c5dcc89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+901b1e8252e21502bc565b4965e2fd0097f8e30a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Babylon.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Babylon.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
