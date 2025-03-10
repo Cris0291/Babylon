01c9628fc625c04a57e3cd065fa54e291dacec7c
Cris0291 <cris0291@gmail.com>
2025-03-10 08:40:59 -0600

    Add infrstructure project to channels module

---
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Infrastructure/Database/ChannelsDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Babylon.Modules.Channels.Infrastructure.Database;
+public sealed class ChannelsDbContext(DbContextOptions<ChannelsDbContext> options): DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Channels);
+    }
+}
---
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Infrastructure/Database/Schemas.cs
@@ -0,0 +1,5 @@
+﻿namespace Babylon.Modules.Channels.Infrastructure.Database;
+internal static class Schemas
+{
+    internal const string Channels = "channels";
+}
