27074e5071b4a042a342f0b1ec91d6a0197df8d9
Cris0291 <cris0291@gmail.com>
2025-03-17 08:37:51 -0600

    Add the unit of work abstraction and the create channel use case

---
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/Channels/CreateChannelCommand.cs
@@ -0,0 +1,4 @@
+﻿using Babylon.Common.Application.Messaging;
+
+namespace Babylon.Modules.Channels.Application.Channels;
+public sealed record CreateChannelCommand() : ICommand<Guid>;
---
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/Channels/CreateChannelCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using Babylon.Common.Application.Messaging;
+using Babylon.Common.Domain;
+
+namespace Babylon.Modules.Channels.Application.Channels;
+internal sealed class CreateChannelCommandHandler : ICommandHandler<CreateChannelCommand, Guid>
+{
+    public Task<Result<Guid>> Handle(CreateChannelCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
