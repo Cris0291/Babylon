9e4a74eaf1ef6e677be98551885f69d4b4e05510
Cris0291 <cris0291@gmail.com>
2025-03-17 08:36:57 -0600

    Add common service registration for the infrastructure module

--- Babylon/src/Common/Babylon.Common.Infrastructure/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Babylon.Common.Infrastructure
-{
-    public class Class1
-    {
-
-    }
-}
--- Babylon/src/Common/Babylon.Common.Infrastructure/obj/Debug/net8.0/Babylon.Common.Infrastructure.AssemblyInfo.cs
+++ Babylon/src/Common/Babylon.Common.Infrastructure/obj/Debug/net8.0/Babylon.Common.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Babylon.Common.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51fc803e97e7b2c6cbe213c5da1fedb1e0527568")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1b78f9b964fe0dd5a1336c477ac2315738dc5a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Babylon.Common.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Babylon.Common.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
