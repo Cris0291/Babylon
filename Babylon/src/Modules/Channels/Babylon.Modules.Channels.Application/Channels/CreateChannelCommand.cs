f6d3cab5af1ddac655721c61b09f0f07fceeaa58
Cris0291 <cris0291@gmail.com>
2025-03-19 20:09:56 -0600

    Add the create channel use case

--- Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/Channels/CreateChannelCommand.cs
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/Channels/CreateChannelCommand.cs
@@ -1,4 +1,4 @@
 ﻿using Babylon.Common.Application.Messaging;
 
 namespace Babylon.Modules.Channels.Application.Channels;
-public sealed record CreateChannelCommand() : ICommand<Guid>;
+public sealed record CreateChannelCommand(string ChannelName, bool IsPublicChannel, Guid MemberId) : ICommand<Guid>;
--- Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/Channels/CreateChannelCommandHandler.cs
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/Channels/CreateChannelCommandHandler.cs
@@ -1,11 +1,26 @@
 ﻿using Babylon.Common.Application.Messaging;
 using Babylon.Common.Domain;
+using Babylon.Modules.Channels.Application.Abstractions.Data;
+using Babylon.Modules.Channels.Domain.Channels;
+using Babylon.Modules.Channels.Domain.Members;
 
 namespace Babylon.Modules.Channels.Application.Channels;
-internal sealed class CreateChannelCommandHandler : ICommandHandler<CreateChannelCommand, Guid>
+internal sealed class CreateChannelCommandHandler(IChannelRepository channelRepository,IMemberRepository memberRepository, IUnitOfWork unitOfWork) : ICommandHandler<CreateChannelCommand, Guid>
 {
-    public Task<Result<Guid>> Handle(CreateChannelCommand request, CancellationToken cancellationToken)
+    public async Task<Result<Guid>> Handle(CreateChannelCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        bool existMember = await memberRepository.Exist(request.MemberId);
+        if (!existMember)
+        {
+            throw new InvalidOperationException("Member was not found");
+        }
+
+        var channel = Channel.CreateChannel(request.ChannelName, request.IsPublicChannel, request.MemberId);
+
+        await channelRepository.Insert(channel);
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return channel.ChannelId;
     }
 }
