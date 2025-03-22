6b18825e8e3dc52dfbdee394c7250d5f7a80344b
Cris0291 <cris0291@gmail.com>
2025-03-21 18:11:39 -0600

    Add the endpoints static class

---
+++ Babylon/src/Common/Babylon.Common.Presentation/Endpoints/ApiEndpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace Babylon.Common.Presentation.Endpoints;
+public static class ApiEndpoints
+{
+    private const string Base = "api";
+
+    public static class Channels
+    {
+        private const string ChannelBase = $"{Base}/channels";
+
+        public const string CreateChannel = ChannelBase;
+        public const string GetChannels = ChannelBase;
+    }
+}
