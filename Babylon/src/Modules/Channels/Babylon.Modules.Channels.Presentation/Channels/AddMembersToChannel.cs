3fbead5fde736f714185cd5252322789520ac94a
Cris0291 <cris0291@gmail.com>
2025-03-25 09:32:45 -0600

    Add delete member endpoint

---
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Presentation/Channels/AddMembersToChannel.cs
@@ -0,0 +1,19 @@
+﻿using Babylon.Common.Presentation.Endpoints;
+using Babylon.Modules.Channels.Application.Channels.AddMembersToChannel;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Babylon.Modules.Channels.Presentation.Channels;
+internal class AddMembersToChannel : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost(ApiEndpoints.Channels.AddMembersToChannel, async (ISender sender, Guid id, Request request) =>
+        {
+            await sender.Send(new AddMembersToChannelCommand(id, request.Members));
+        }).WithTags(Tags.Channels);
+    }
+    internal sealed record Request(IEnumerable<Guid> Members);
+}
---
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Presentation/Channels/DeleteMemeberFromChannel.cs
@@ -0,0 +1,18 @@
+﻿using Babylon.Common.Presentation.Endpoints;
+using Babylon.Modules.Channels.Application.Channels.DeleteMemberFormChannel;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Babylon.Modules.Channels.Presentation.Channels;
+internal sealed class DeleteMemeberFromChannel : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapDelete(ApiEndpoints.Channels.DeleteMemberFromChannel, async (ISender sender, Guid channelId, Guid memberId) =>
+        {
+            await sender.Send(new DeleteMemberFormChannelCommand(channelId, memberId));
+        }).WithTags(Tags.Channels);
+    }
+}
