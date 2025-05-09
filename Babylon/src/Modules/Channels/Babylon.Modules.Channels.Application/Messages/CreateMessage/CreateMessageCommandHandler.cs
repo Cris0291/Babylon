294f9a122c9e06aa87a1fe8fcb78789186189b26
Cris0291 <cris0291@gmail.com>
2025-05-08 19:01:24 -0600

    Add changes to the channels application project

--- Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/Messages/CreateMessage/CreateMessageCommandHandler.cs
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/Messages/CreateMessage/CreateMessageCommandHandler.cs
@@ -1,9 +1,10 @@
 ﻿using Babylon.Common.Application.Messaging;
 using Babylon.Common.Domain;
+using Babylon.Modules.Channels.Application.Abstractions.Data;
 using Babylon.Modules.Channels.Domain.MessageChannels;
 
 namespace Babylon.Modules.Channels.Application.Messages.CreateMessage;
-internal sealed class CreateMessageCommandHandler(IMessageChannelRepository messageChannelRepository) : ICommandHandler<CreateMessageCommand>
+internal sealed class CreateMessageCommandHandler(IMessageChannelRepository messageChannelRepository, IUnitOfWork unitOfWork) : ICommandHandler<CreateMessageCommand>
 {
     public async Task<Result> Handle(CreateMessageCommand request, CancellationToken cancellationToken)
     {
@@ -16,6 +17,8 @@ internal sealed class CreateMessageCommandHandler(IMessageChannelRepository mess
             request.Avatar);
 
         await messageChannelRepository.Insert(message);
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
 
         return Result.Success();
     }
--- Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/ThreadMessages/CreateThreadMessage/CreateThreadMessageCommandHandler.cs
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/ThreadMessages/CreateThreadMessage/CreateThreadMessageCommandHandler.cs
@@ -1,11 +1,25 @@
 ﻿using Babylon.Common.Application.Messaging;
 using Babylon.Common.Domain;
+using Babylon.Modules.Channels.Application.Abstractions.Data;
+using Babylon.Modules.Channels.Domain.MessageThreadChannels;
 
 namespace Babylon.Modules.Channels.Application.ThreadMessages.CreateThreadMessage;
-internal sealed class CreateThreadMessageCommandHandler : ICommandHandler<CreateThreadMessageCommand>
+internal sealed class CreateThreadMessageCommandHandler(IMessageThreadChannelRepository repository, IUnitOfWork unitOfWork) : ICommandHandler<CreateThreadMessageCommand>
 {
     public async Task<Result> Handle(CreateThreadMessageCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var message = MessageThreadChannel.Create(
+            request.UserName,
+            request.Message,
+            request.ThreadId,
+            request.MemberId,
+            request.PublicationDate
+            );
+
+        await repository.Insert(message);
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
     }
 }
