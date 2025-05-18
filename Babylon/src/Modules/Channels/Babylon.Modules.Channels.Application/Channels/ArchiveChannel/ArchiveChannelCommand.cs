d0cf293bf0e2d5a2f39b9fe69e56623077b98c7a
Cris0291 <cris0291@gmail.com>
2025-05-17 19:43:43 -0600

    Add archive channel use case

---
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/Channels/ArchiveChannel/ArchiveChannelCommand.cs
@@ -0,0 +1,4 @@
+﻿using Babylon.Common.Application.Messaging;
+
+namespace Babylon.Modules.Channels.Application.Channels.ArchiveChannel;
+public record ArchiveChannelCommand(Guid ChannelId) : ICommand;
---
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/Channels/ArchiveChannel/ArchiveChannelCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Babylon.Common.Application.Messaging;
+using Babylon.Common.Domain;
+using Babylon.Modules.Channels.Application.Abstractions.Data;
+using Babylon.Modules.Channels.Domain.Channels;
+
+namespace Babylon.Modules.Channels.Application.Channels.ArchiveChannel;
+internal sealed class ArchiveChannelCommandHandler(IUnitOfWork unitOfWork, IChannelRepository channelRepository) : ICommandHandler<ArchiveChannelCommand>
+{
+    public async Task<Result> Handle(ArchiveChannelCommand request, CancellationToken cancellationToken)
+    {
+        Channel? channel = await channelRepository.GetChannel(request.ChannelId);
+
+        if (channel is null)
+        {
+            throw new InvalidOperationException("Channel was not found");
+        }
+
+        channel.ArchiveChannel();
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
