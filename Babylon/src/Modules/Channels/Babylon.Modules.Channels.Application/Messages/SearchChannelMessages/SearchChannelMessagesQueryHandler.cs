0f158bbde8cc0ef1b1627ff3ac0bcc8feed1edd3
Cris0291 <cris0291@gmail.com>
2025-06-02 19:29:19 -0600

    Add search messages use case

--- Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/Messages/SearchChannelMessages/SearchChannelMessagesQueryHandler.cs
+++ Babylon/src/Modules/Channels/Babylon.Modules.Channels.Application/Messages/SearchChannelMessages/SearchChannelMessagesQueryHandler.cs
@@ -2,6 +2,7 @@
 using Babylon.Common.Application.Data;
 using Babylon.Common.Application.Messaging;
 using Babylon.Common.Domain;
+using Dapper;
 
 namespace Babylon.Modules.Channels.Application.Messages.SearchChannelMessages;
 internal sealed class SearchChannelMessagesQueryHandler(IDbConnectionFactory dbConnectionFactory) : IQueryHandler<SearchChannelMessagesQuery, IEnumerable<SearchedChannelMessageDto>>
@@ -10,10 +11,39 @@ internal sealed class SearchChannelMessagesQueryHandler(IDbConnectionFactory dbC
     {
         await using DbConnection connection = await dbConnectionFactory.OpenConnectionAsync();
 
-        const string sql = 
+        const string sql =
             """
-            SELECT
-            FROM
+            WITH SelectedChannel AS (
+                SELECT channel_id, type
+                FROM channels.channels
+                WHERE channel_id = @ChannelId
+            ),
+            Authorized AS (
+                SELECT
+                   sc.channel_id,
+                   sc.type,
+                   CASE
+                       WHEN sc.type = 'Public' THEN 1
+                       WHEN EXISTS (
+                           SELECT 1
+                           FROM channels.channel_members CM
+                           WHERE cm.channel_id = @ChannelId AND cm.id = @Id
+                       ) THEN 1
+                       ELSE 0
+                   END AS IsAuthorized
+                FROM SelectedChannel sc
+            ),
+            FilteredMessages AS (
+                SELECT mc.*
+                FROM channels.message_channels mc
+                JOIN  Authorized a ON a.channel_id = mc.channel_id
+                WHERE a.IsAuthorized = 1
+                   AND m.message LIKE '%' + @Search + '%'
+            )
+            SELECT *
+            FROM FilteredMessages
             """;
+
+        IEnumerable<SearchedChannelMessageDto> messages = await connection.QueryAsync<SearchedChannelMessageDto>(sql, new { request.ChannelId, request.Id, request.Search});
     }
 }
